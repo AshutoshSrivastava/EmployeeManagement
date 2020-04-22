c6a12b3a070a55af221a9c10e7902ea36a203a8c
Ashutosh Srivastava <erashu@live.in>
2020-04-22 07:37:36 +0530

    parameter added for show hide employee card action

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeDetailsBase : ComponentBase
     {
+        protected string Coordinates { get; set; }
         public Employee Employee { get; set; } = new Employee();
 
         [Inject]
@@ -22,6 +24,11 @@ namespace EmployeeManagement.Web.Pages
         {
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"x = {e.ClientX}, y = {e.ClientY}";
         }
     }
 }
