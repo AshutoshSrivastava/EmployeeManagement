a670e2474f2a1ca6e72b6bd888db0bda5638577f
Ashutosh Srivastava <erashu@live.in>
2020-04-29 11:58:55 +0530

    Edit employee form

--- EmployeeManagement.Web/Startup.cs
+++ EmployeeManagement.Web/Startup.cs
@@ -34,6 +34,10 @@ namespace EmployeeManagement.Web
             {
                 client.BaseAddress = new Uri("https://localhost:44310/");
             });
+            services.AddHttpClient<IDepartmentService, DepartmentService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44310/");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
